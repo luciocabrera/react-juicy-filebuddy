5499b4b7cdaa9db0f524aad4274c1e2724a42db0
luciocabrera <lucio.cabrera@just-bi.nl>
2019-04-01 11:36:08 +0200

    added pagination component

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './../../App.css';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+import "./../../assets/css/TableStyles.css";
+import TablePagination from './../TablePagination/TablePagination';
 const Table = ({ data, columns, defaultPageSize,className }) => (
+
   <div>
     <ReactTable
       data={data}
       columns={columns}
       defaultPageSize={defaultPageSize}
+      PaginationComponent={TablePagination}
       className={className}
     />
   </div>
